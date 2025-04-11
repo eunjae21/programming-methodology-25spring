fee08b652c4027f4ecb321a809eda05c9098f9b6
eunjae <leejae527@naver.com>
2025-04-11 12:51:17 +0900

    "출석 과제"

--- calculator.cpp
+++ calculator.cpp
@@ -2,17 +2,46 @@
 #include <stdexcept>
 
 int Calculator::add(int a, int b) {
-    // TODO
+    int result = a+b;
+    if((a>0 && b>0) && result<0){
+        throw std::overflow_error("overflow");
+    }
+    if((a<0 && b<0) && result>0){
+        throw std::overflow_error("underflow");
+    }
+    return result;
 }
 
 int Calculator::sub(int a, int b) {
-    // TODO
+    int result = a-b;
+
+    if(a>b && result<0){
+        throw std::overflow_error("overflow");
+    }
+    else if(a<b && result>0){
+        throw std::overflow_error("underflow");
+    }
+    return result;
 }
 
 int Calculator::mul(int a, int b) {
-    // TODO
+    int result = a*b;
+
+    if((result<0) && ((a>0 && b>0) || (a<0 && b<0))){
+        throw std::overflow_error("overflow");
+    }
+    else if((result>0) && ((a>0 && b<0) || (a<0 && b>0))){
+        throw std::overflow_error("underflow");
+    }
+    return result;
 }
 
 int Calculator::div(int a, int b) {
-    // TODO
+    if(b==0){
+        throw std::invalid_argument("division by zero");
+    }
+    else if(a == -2147483648 && b == -1){
+        throw std::overflow_error("overflow");
+    }
+    return a/b;
 }
